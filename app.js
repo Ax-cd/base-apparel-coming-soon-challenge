84a35e0ff9b085606d153253979e0f68da866100
Ax-cd <axcd.coder@gmail.com>
2022-05-12 21:55:36 +0200

    save progress and changes made so far

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hello World!");
+
+  const form = document.getElementById("form");
+  const email = document.getElementById("email");
+  let input = email.value;
+  //error msg "Please provide a valid email" + error icon when input field is empty || email address not formatted correctly
+
+  form.addEventListener("submit", (event) => {
+    // event.preventDefault();
+    console.log(input);
+  });
+
+  // checkValidEmail = (input) => {
+  //   let
+  // };
+
+  // let errorMsg = document.createElement("span");
+  // errorMsg.id = "error-msg";
+  // errorMsg.innerHTML = "Please provide a valid email";
+  // document.body.appendChild(errorMsg);
+});
