8b394f7e0d63fb46f86dd0fb9b2b5497e3976c24
Ax-cd <axcd.coder@gmail.com>
2022-07-22 12:09:16 +0200

    add new css file; sass + js not working

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("form");
   const email = document.getElementById("email");
+  const btn = document.getElementById("btn");
 
-  form.addEventListener("submit", (event) => {
+  // working
+  btn.addEventListener("click", function (event) {
     event.preventDefault(); //stop form submission
     let emailAdress = email.value;
-
     if (!checkValid(emailAdress) || email === "") {
-      form.classList.add("error");
+      email.classList.add("error-input");
+      form.classList.add("error-msg");
     } else {
-      form.classList.remove("error");
+      email.classList.remove("error-input");
+      form.classList.remove("error-msg");
     }
   });
-
   function checkValid(email) {
     const re =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
+
+  // // not working... but why?
+  // function submitEmail(event) {
+  //   let emailAdress = email.value;
+  //   if (!checkValid(emailAdress) || email === "") {
+  //     email.classList.add("error-input");
+  //     form.classList.add("error-msg");
+  //     console.log("invalid email");
+  //   }
+  //   event.preventDefault();
+  // }
+  // form.addEventListener("submit", submitEmail);
 });
